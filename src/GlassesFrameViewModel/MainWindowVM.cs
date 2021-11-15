c411dbcaba465d639b44c6ba6c46efe3415f7f46
Alexandra Dyagay <dyagaay@gmail.com>
2021-11-15 20:58:15 +0700

    Добавлены методы в класс InventorConnector

---
+++ src/GlassesFrameViewModel/MainWindowVM.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GlassesFrame;
+using GlassesFrameViewModel.Service;
+using InventorApi;
+
+namespace GlassesFrameViewModel
+{
+    public class MainWindowVM : ViewModelBase
+    {
+        private GlassesFrameParameters _glassesFrameParameters;
+
+        private GlassesFrameBuilder _glassesFrameBuilder;
+
+        private IMessageBoxService _messageBoxService;
+
+        public GlassesFrameParameters GlassesFrameParameters
+        {
+            get
+            {
+                return _glassesFrameParameters;
+            }
+            set
+            {
+                _glassesFrameParameters = value;
+                RaisePropertyChanged(nameof(GlassesFrameParameters));
+            }
+        }
+
+        public GlassesFrameBuilder GlassesFrameBuilder
+        {
+            get
+            {
+                return _glassesFrameBuilder;
+            }
+            set
+            {
+                _glassesFrameBuilder = value;
+                RaisePropertyChanged(nameof(GlassesFrameBuilder));
+            }
+        }
+
+        public RelayCommand ApplyCommand { get; set; }
+
+        public MainWindowVM(IMessageBoxService messageBoxService)
+        {
+            ApplyCommand = new RelayCommand(Apply);
+        }
+
+        private void Apply()
+        {
+
+        }
+    }
+}
--- src/InventorApi/InventorConnector.cs
+++ src/InventorApi/InventorConnector.cs
@@ -1,12 +1,99 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using Inventor;
+using Application = Inventor.Application;
 
 namespace InventorApi
 {
     public class InventorConnector
     {
+        public Application InventorApplication { get; set; }
+
+        public PartDocument PartDocument { get; set; }
+
+        public PartComponentDefinition PartDefinition { get; set; }
+
+        public TransientGeometry TransientGeometry { get; set; }
+
+        private PlanarSketch Sketch { get; set; }
+
+        public ObjectCollection CreateObjectCollection()
+        {
+            return InventorApplication.TransientObjects.CreateObjectCollection();
+        }
+
+        public void CreateNewDocument()
+        {
+            InventorApplication = null;
+
+            try
+            {
+                InventorApplication = (Application)Marshal.GetActiveObject("Inventor.Application");
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    Type _inventorApplicationType = Type.GetTypeFromProgID("Inventor.Application");
+
+                    InventorApplication = (Application)Activator.CreateInstance(_inventorApplicationType);
+                    InventorApplication.Visible = true;
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Не получилось запустить Inventor.");
+                }
+            }
+
+            PartDocument = (PartDocument)InventorApplication.Documents.Add
+            (DocumentTypeEnum.kPartDocumentObject,
+                InventorApplication.FileManager.GetTemplateFile
+                (DocumentTypeEnum.kPartDocumentObject,
+                    SystemOfMeasureEnum.kMetricSystemOfMeasure));
+
+            PartDefinition = PartDocument.ComponentDefinition;
+            TransientGeometry = InventorApplication.TransientGeometry;
+        }
+
+        public PlanarSketch MakeNewSketch(double offset)
+        {
+            //[1 - ZY; 2 - ZX; 3 - XY]
+            int planeNumber = 1;
+            var mainPlane = PartDefinition.WorkPlanes[planeNumber];
+            var offsetPlane = PartDefinition.WorkPlanes.AddByPlaneAndOffset(mainPlane, offset);
+            var sketch = PartDefinition.Sketches.Add(offsetPlane);
+            return sketch;
+        }
+
+        public void DrawCircle(Point2d centerPoint, double diameter)
+        {
+            Sketch.SketchCircles.AddByCenterRadius(centerPoint, diameter);
+        }
+
+        public void DrawRectangle(Point2d pointOne, Point2d pointTwo)
+        {
+            Sketch.SketchLines.AddAsTwoPointRectangle(pointOne, pointTwo);
+        }
+
+        public void DrawLine(Point2d startPoint, Point2d endPoint)
+        {
+            Sketch.SketchLines.AddByTwoPoints(startPoint, endPoint);
+        }
+
+        public void Extrude(double distance)
+        {
+            var sketchProfile = Sketch.Profiles.AddForSolid();
+            var extrudeDef = PartDefinition.Features.ExtrudeFeatures.CreateExtrudeDefinition(sketchProfile,
+                PartFeatureOperationEnum.kJoinOperation);
+            extrudeDef.SetDistanceExtent(distance, PartFeatureExtentDirectionEnum.kPositiveExtentDirection);
+            PartDefinition.Features.ExtrudeFeatures.Add(extrudeDef);
+        }
+
+
     }
 }
